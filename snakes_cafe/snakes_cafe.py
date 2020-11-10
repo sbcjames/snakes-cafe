a196c7626f936fa6091f8a26d36a3a6f94104a24
James Swift <sbcjames86@yahoo.com>
2020-11-09 23:33:49 -0800

    initial work on order system

---
+++ snakes_cafe/snakes_cafe.py
@@ -0,0 +1,14 @@
+print("*"*38),
+print("**   Welcome to the Snakes Cafe!   **\n**    Please see our menu below.    **\n**\n** To quit at any time, type \"quit\" **"),
+print("*"*38),
+print("\nAppetizers\n----------\nWings\nCookies\nSpring Rolls\n"),
+print("Entrees\n----------\nSalmon\nSteak\nMeat Tornado\nA Literal Garden\n"),
+print("Desserts\n----------\nIce Cream\nCake\nPie\n"),
+print("Drinks\n----------\nCoffee\nTea\nUnicorn Tears\n"),
+print("*"*36),
+print("** What would you like to order? **"),
+print("*"*36),
+response = input("> "),
+
+response = 
+
